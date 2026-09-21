37107d45a4f0097bdb31cdad27d346ce317705b6
ondraborec123 <selucky.o@ssmg.cz>
2026-09-21 09:36:57 +0200

    added vista.css

--- themenavi.js
+++ themenavi.js
@@ -2,5 +2,6 @@ document.querySelector(".themechooser").innerHTML = `
   <small>select css:
 				<button onclick="toggleTheme('/hacker.css')">hacker</button>
 				<button onclick="toggleTheme('/95.css')">95</button>
+				<button onclick="toggleTheme('/vista.css')">vista</button>
 	</small>
 `;
